f994be56a9b0c66421edf8a4ec57e794f4685e56
xdopson1778 <xdopsonnom@gmail.com>
2023-05-26 16:24:51 -0700

    simple

---
+++ errors.c
@@ -0,0 +1,80 @@
+
+#include "shell.h"
+
+/**
+ * str - the string to be printed
+ * _eputs - this prints an input string
+ * Return - Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * c - The character to print
+ * _eputchar - this writes the character c to stderr
+ * Return - On success 1. On error, -1 is returned, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * c - The character to print
+ * fd - The filedescriptor to write to
+ *  _putfd - this writes the character c to given fd
+ * Return - On success 1.On error, -1 is returned, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * str - the string to be printed
+ * fd - the filedescriptor to write to
+ *  _putsfd - this prints an input string
+ * Return: the number of chars put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
